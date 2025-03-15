bae44634d82f57561fba1c544f29e14113797cc2
Abhishek <abhishekdelris@gmail.com>
2025-03-15 12:36:53 +0530

    changes_15/03

---
+++ src/pages/profile_details/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next';
+import ProflieDetails from '../../components/Profile_details'; 
+
+
+const Home: NextPage = () => {
+  return (
+   
+    <ProflieDetails />
+
+    
+  );
+};
+
+export default Home;
